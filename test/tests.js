755fe49c6d44a328142f6b449a4e252544dd9cbe
Cedonia Peterson <cedoniap@google.com>
2020-08-07 14:21:45 -0400

    add

---
+++ test/tests.js
@@ -0,0 +1,9 @@
+const { describe, it, beforeEach, afterEach } = require('mocha');
+const assert = require('assert');
+
+
+describe('tests', () => {
+    it('is a test', () => {
+        assert(true);
+    });
+});
